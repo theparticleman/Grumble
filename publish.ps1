97f879f4e1d7b3599b4b2a22d966aa087f49d1c8
Jon Turner <theparticleman@gmail.com>
2022-04-01 11:14:11 -0600

    Add MacOS and Linux builds to publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,7 @@
 dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
+dotnet publish -p:PublishSingleFile=true -r osx-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
+dotnet publish -p:PublishSingleFile=true -r linux-x64 -c Release --self-contained true -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true
+
+Compress-Archive .\bin\Release\net6.0\win-x64\publish\Grumble.exe Grumble_win-x64.zip
+Compress-Archive .\bin\Release\net6.0\osx-x64\publish\Grumble Grumble_osx-x64.zip
+Compress-Archive .\bin\Release\net6.0\linux-x64\publish\Grumble Grumble_linux-x64.zip
